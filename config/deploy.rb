bb58ae57ebe0064603c4f969c79f021794abc247
Fernando <fernando93d@gmail.com>
2020-11-24 13:15:55 -0600

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,18 +8,19 @@ set :branch, "Laravel-8"
 
 set :deploy_to, '/var/www/laravel-capistrano'
 
-set :keep_releases, 2
+set :laravel_dotenv_file, '/var/www/secrets/.env'
 
-set :laravel_env_file, '/var/www/secrets/.env'
+set :keep_releases, 5
 
 append  :linked_dirs, 
         'storage/app',
         'storage/framework/cache',
         'storage/framework/sessions',
+        'storage/framework/views',
         'storage/logs'
 
 namespace :composer do
-    desc "Install composer"
+    desc "Running Composer Install"
     task :install do
         on roles(:composer) do
             within release_path do
@@ -28,65 +29,22 @@ namespace :composer do
         end
     end
 end
-
 namespace :laravel do
-    task :fix_permissions do
+    task :fix_permission do
         on roles(:laravel) do
-            execute :sudo, "chmod", "-R ug+rwx #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
-            execute :sudo, :chgrp, "-R www-data #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
-        end
-    end 
-
-    task :configure_env do
-        dotenv = fetch(:laravel_env_file)
-        on roles(:laravel) do
-            execute :cp, "#{dotenv} #{release_path}/.env"
+            execute :chmod, "-R ug+rwx #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
+            execute :chgrp, "-R www-data #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
         end
     end
-
-    task :clean_views do
-        on roles(:laravel) do
-            execute "cd #{release_path} php artisan view:clear"
+    task :configure_dot_env do
+    dotenv_file = fetch(:laravel_dotenv_file)
+        on roles (:laravel) do
+        execute :cp, "#{dotenv_file} #{release_path}/.env"
         end
     end
 end
-
-namespace :deploy do 
+namespace :deploy do
     after :updated, "composer:install"
-    after :updated, "laravel:fix_permissions"
-    after :updated, "laravel:configure_env"
+    after :updated, "laravel:fix_permission"
+    after :updated, "laravel:configure_dot_env"
 end
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
